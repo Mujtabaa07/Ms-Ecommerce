03c05fc7c884d305c6fc3f0fc3a0a7ed7836a6bf
Mohamed Mujtaba <mohamedmujtaba07@gmail.com>
2024-11-27 00:18:08 +0530

    updatedadadaaaaakdadad

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: process.env.NODE_ENV === 'production'
-    ? ['https://ms-ecommerce-mujtabaa07s-projects.vercel.app']
+    ? ['https://ms-ecommerce-theta.vercel.app/']
     : ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
